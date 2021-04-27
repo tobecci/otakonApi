6a4a8cdc7271765a714e3462842a74b1040dcd5e
tobecci <ojinakatochukwu@gmail.com>
2021-04-27 22:49:48 +0100

    fix: enable cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const controllers = require("./src/controllers.js");
 const port = process.env.PORT || 3000;
 const models = require("./src/models.js");
 
+
 models().then((models) => {
+    app.use(cors());
     controllers(app, models);
     console.log({port:port})
     app.listen(port,()=>{
