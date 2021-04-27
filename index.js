094fb34a026f3de4582d837c7f68cf1f0b044548
tobecci <ojinakatochukwu@gmail.com>
2021-04-27 13:43:34 +0100

    made port dynamic

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const controllers = require("./src/controllers.js");
+const port = process.env.PORT || 3000;
 
+console.log({port:port})
 // init controllers
 controllers(app);
-
-
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("app served on localhost:3000");
 });
